6605145229b55191391a880ed35f2bccb6167299
Paul P <85466221+pauljhp@users.noreply.github.com>
2022-07-12 11:00:30 +0800

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
-git clone https://github.com/pauljhp/StockSentimentAnalysis
-cd ./StockSentimentAnalysis
-bash ./setup.sh
+#!/bin/bash
+# setup environment
+conda env create -f ./environment.yml
+
+# --or--
+# conda create --name sent_env --file requirements.txt
+conda activate sent_env
+
+!mkdir -p ./data
+
+# then download the sql files:
+!gdown https://drive.google.com/file/d/1YG_AQIbcY2Mi-bKMLN1hff66jDBFeh4O/view?usp=sharing -O ./data/spx_news_sentiment_fundamental.db
+!gdown https://drive.google.com/file/d/1C49ElctSD0hPsukQTkioneA7PeWBlMfe/view?usp=sharing -O ./data/spx_news_sentiment_price.db
