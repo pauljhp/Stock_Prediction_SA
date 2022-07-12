f4ae99ea6c381c87a0070bdb68d8dda9e243b64b
ppipxam <p.peng@impaxam.com>
2022-07-12 16:10:43 +0800

    fixed setup.sh and environment.yml

--- setup.sh
+++ setup.sh
@@ -12,10 +12,10 @@ fi
 
 # --or--
 # conda create --name sent_env --file requirements.txt
-conda activate sent_env
 
-!mkdir -p ./data
+
+mkdir -p ./data
 
 # then download the sql files:
-!gdown --fuzzy https://drive.google.com/file/d/1YG_AQIbcY2Mi-bKMLN1hff66jDBFeh4O/view?usp=sharing -O ./data/spx_news_sentiment_fundamental.db
-!gdown --fuzzy https://drive.google.com/file/d/1C49ElctSD0hPsukQTkioneA7PeWBlMfe/view?usp=sharing -O ./data/spx_news_sentiment_price.db
+gdown --fuzzy https://drive.google.com/file/d/1YG_AQIbcY2Mi-bKMLN1hff66jDBFeh4O/view?usp=sharing -O ./data/spx_news_sentiment_fundamental.db
+gdown --fuzzy https://drive.google.com/file/d/1C49ElctSD0hPsukQTkioneA7PeWBlMfe/view?usp=sharing -O ./data/spx_news_sentiment_price.db
